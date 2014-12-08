2de846b6eec33d965493671d7c6c2c0dcd0b1138
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2014-12-08 16:15:05 +0100

    it compiles...

--- lib/RDF/Generator/HTTP.pm
+++ lib/RDF/Generator/HTTP.pm
@@ -4,11 +4,78 @@ use warnings;
 
 package RDF::Generator::HTTP;
 
+
 our $AUTHORITY = 'cpan:KJETILK';
 our $VERSION   = '0.001';
 
-1;
+use Moo;
+use Carp qw(carp);
+use RDF::Trine qw(statement blank iri literal);
+use URI::NamespaceMap;
+use Types::Standard qw(InstanceOf);
 
+has message => (is => 'ro', isa => InstanceOf['HTTP::Message'], required => 1);
+
+has ns => (is => 'ro', isa => InstanceOf['URI::NamespaceMap'], lazy => 1, builder => '_build_namespacemap');
+
+sub _build_namespacemap {
+	my $self = shift;
+	return URI::NamespaceMap->new({ rdf => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+											http => 'http://www.w3.org/2007/ont/http#',
+											httph => 'http://www.w3.org/2007/ont/httph#' });
+}
+
+
+sub generate {
+	my $self = shift;	
+	my $model = shift || RDF::Trine::Model->temporary_model;
+	my $reqsubj = blank();
+	my $ressubj = blank();
+	my $ns = $self->ns;
+	if ($self->message->isa('HTTP::Request')) {
+		$self->_request_statements($model, $self->message, $reqsubj);
+		$self->message->headers->scan(sub {
+				my ($field, $value) = @_;
+				$model->add_statement(statement($reqsubj, iri($ns->httph->iri(_fix_headers($field))), literal($value)));
+			 });
+	} elsif ($self->message->isa('HTTP::Response')) {
+		$model->add_statement(statement($ressubj, iri($ns->uri('rdf:type')), iri($ns->uri('http:ResponseMessage'))));
+		$model->add_statement(statement($ressubj, iri($ns->uri('http:status')), literal($self->message->code)));
+		$self->message->headers->scan(sub {
+				  my ($field, $value) = @_;
+				  $model->add_statement(statement($reqsubj, iri($ns->httph->iri(_fix_headers($field))), literal($value)));
+			   });
+		if ($self->message->request) {
+			$model->add_statement(statement($reqsubj, iri($ns->uri('http:hasResponse')), $ressubj));
+			$self->_request_statements($model, $self->message->request, $reqsubj);
+			$self->message->request->headers->scan(sub {
+				my ($field, $value) = @_;
+				$model->add_statement(statement($reqsubj, iri($ns->httph->iri(_fix_headers($field))), literal($value)));
+			 });
+		}
+	} else {
+		carp "Don't know what to do with message object of class " . ref($self->message);
+	}
+	return $model;
+}
+
+sub _request_statements {
+	my ($self, $model, $r, $subj) = @_;
+	my $ns = $self->ns;
+	$model->add_statement(statement($subj, iri($ns->uri('rdf:type')), iri($ns->uri('http:RequestMessage'))));
+	$model->add_statement(statement($subj, iri($ns->uri('http:method')), literal('GET')));
+	$model->add_statement(statement($subj, iri($ns->uri('http:requestURI')), iri($r->uri)));
+}
+
+sub _fix_headers {
+	my $field = shift;
+	$field =~ tr/_/-/;
+	$field = lc $field;
+	return $field;
+}
+	  
+
+1;
 __END__
 
 =pod
@@ -22,6 +89,25 @@ RDF::Generator::HTTP - Generate RDF from a HTTP message
 =head1 SYNOPSIS
 
 =head1 DESCRIPTION
+
+=head2 Constructor
+
+=over
+
+=item C<< new(%attributes) >>
+
+Moose-style constructor function.
+
+=back
+
+=head2 Attributes
+
+These attributes may be passed to the constructor.
+
+=item C<< message >>
+
+A L<HTTP::Message> (or subclass thereof) object to generate RDF for. Required.
+
 
 =head1 BUGS
 
